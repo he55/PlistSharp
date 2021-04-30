99de245854b80e01d205949e9458a9ab66875f4b
he55 <40764272+he55@users.noreply.github.com>
2021-04-30 15:03:26 +0800

    delete

--- build.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-set PACKDIR="packages"
-
-mkdir %PACKDIR%
-dotnet pack -c Release -o %PACKDIR% PlistSharp/PlistSharp.csproj
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-PACKDIR="packages"
-
-mkdir -p $PACKDIR
-dotnet pack -c Release -o $PACKDIR PlistSharp/PlistSharp.csproj
